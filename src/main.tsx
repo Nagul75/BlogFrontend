23fd34af147d897af5e81ccd0eba211cda7e95a9
Nagul Kanna T <nagulk697@gmail.com>
2025-12-05 23:13:31 +0530

    Add basic theme toggle function

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Routes } from "./Routes";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "./components/theme-provider";
 
 const router = createBrowserRouter(Routes);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 );
