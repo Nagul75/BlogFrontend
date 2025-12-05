4d10ab3642f0cfefff609fe627ed7e30ebe6d4af
Nagul Kanna T <nagulk697@gmail.com>
2025-12-05 22:52:59 +0530

    Create basic routing

---
+++ src/Routes.tsx
@@ -0,0 +1,8 @@
+import App from "./pages/App";
+
+export const Routes = [
+  {
+    path: "/",
+    element: <App />,
+  },
+];
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Button } from './components/ui/button'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Routes } from "./Routes";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(Routes);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className='min-h-screen min-w-screen flex items-center justify-center'>
-      <Button>Click me!</Button>
-    </div>
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
