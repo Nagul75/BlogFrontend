57b1b5bae8f86799b09acdad888ec20e39cdbbb0
Nagul Kanna T <nagulk697@gmail.com>
2025-12-09 15:26:44 +0530

    BLG-10 test GET post and comments api

--- src/utils/apiTest.ts
+++ src/utils/apiTest.ts
@@ -2,10 +2,14 @@ import apiClient from "./apiClient.ts";
 
 async function testApi() {
   try {
-    const response = await apiClient.get("/posts/");
-
-    console.log("Status:", response.status);
+    const response = await apiClient.get(
+      "/posts/this-is-the-third-posts-ever-in-this-app-isnt-that-cool/",
+    );
+    const comments = await apiClient.get(
+      "/posts/this-is-the-third-posts-ever-in-this-app-isnt-that-cool/comments/",
+    );
     console.log("Data:", response.data);
+    console.log("Comments: ", comments.data);
   } catch (error) {
     console.error(error);
   }
